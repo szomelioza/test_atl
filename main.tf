e8b168cdd0d0685233483975c2ef838779c0b8e4
szomelioza <123871372+szomelioza@users.noreply.github.com>
2023-06-26 18:46:16 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,16 @@
-resource "null_resource" "example1" {
+terraform {
+  backend "s3" {
+    bucket = "terraform-states-999"
+    region = "eu-central-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_sqs_queue" "my_queue" {
+  name                      = "dummy-q"
+  visibility_timeout_seconds = 300
+  message_retention_seconds = 86400
 }
